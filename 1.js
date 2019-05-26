41c37e09069ce7b9662f5ae8aed90bb7dfabec78
thuongd0 <thuongdo260296@gmail.com>
2019-05-26 11:04:45 +0700

    Update 1.js

--- 1.js
+++ 1.js
@@ -1,32 +1,50 @@
-﻿document.addEventListener("DOMContentLoaded", function(){
-    //scroll menu 
-    var trangthai = 'duoi200';
-    var doituongmenu = document.querySelector('.menu');
-    var phantuload = document.querySelector('.project');
-    var trangthai_title = 'duoi';
-    var vitri = phantuload.offsetTop - 400;
-    console.log(doituongmenu);
-    window.addEventListener('scroll',function(){
-		console.log(window.pageYOffset);
-		if(window.pageYOffset > 200){
-			if(trangthai == 'duoi200'){
-				trangthai = 'tren200';
-				doituongmenu.classList.add('bg');
-			}
+﻿﻿document.addEventListener("DOMContentLoaded", function(){
+	//scroll menu 
+	var trangthai = 'duoi200';
+	var doituongmenu = document.querySelector('.menu');
+	var phantuload = document.querySelector('.project');
+	var trangthai_title = 'duoi';
+	var vitri = phantuload.offsetTop - 400;
+	console.log(doituongmenu);
+	window.addEventListener('scroll',function(){
+	console.log(window.pageYOffset);
+	if(window.pageYOffset > 200){
+		if(trangthai == 'duoi200'){
+			trangthai = 'tren200';
+			doituongmenu.classList.add('bg');
 		}
-		else if(window.pageYOffset<=200){
-			if(trangthai == 'tren200'){
-				trangthai = 'duoi200';
-				doituongmenu.classList.remove('bg');
+	}
+	else if(window.pageYOffset<=200){
+		if(trangthai == 'tren200'){
+			trangthai = 'duoi200';
+			doituongmenu.classList.remove('bg');
+		}
 			}
+})	
+})
+
+$(function(){
+	new WOW().init();
+})
+
+$(document).ready(function(){
+    $('.owl-carousel').owlCarousel({
+        margin:10,
+        nav:true,
+        navText: [
+            "<i class='fas fa-arrow-left'></i>",
+            "<i class='fas fa-arrow-right'></i>"
+          ],
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:3
+            },
+            1000:{
+                items:3
+            }
         }
-        
-        		//xu ly phan tu load
-		if(window.pageYOffset > vitri){
-			if(trangthai_title == 'duoi'){
-				trangthai_title = 'tren';
-				phantuload.classList.add('dilen');
-			}
-		}
-	})	
+    })
 })
